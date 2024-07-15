acd97ba956703e53291b4e778bd654054be651e2
Deepak Singh <deepaksingh101101@gmail.com>
2024-07-15 14:37:38 +0530

    custom data datbe comented

--- components/tables/product-management-tables/customDataTable.tsx
+++ components/tables/product-management-tables/customDataTable.tsx
@@ -1,22 +1,22 @@
-// CustomDataTable.tsx
+// // CustomDataTable.tsx
 
-import React from 'react';
-import { DataTable, DataTableProps } from '@tanstack/react-table';
-import { UserManagement } from '@/constants/user-management-data';
-import { ColumnDef } from '@tanstack/react-table';
-import { ProductManagement } from '@/constants/product-management-data';
+// import React from 'react';
+// import { DataTable, DataTableProps } from '@tanstack/react-table';
+// import { UserManagement } from '@/constants/user-management-data';
+// import { ColumnDef } from '@tanstack/react-table';
+// import { ProductManagement } from '@/constants/product-management-data';
 
-interface CustomDataTableProps extends DataTableProps<ProductManagement, unknown> {
-  onSearch: (searchValue: string) => void;
-  onSort: (sortBy: keyof ProductManagement, sortOrder: 'asc' | 'desc') => void;
-}
+// interface CustomDataTableProps extends DataTableProps<ProductManagement, unknown> {
+//   onSearch: (searchValue: string) => void;
+//   onSort: (sortBy: keyof ProductManagement, sortOrder: 'asc' | 'desc') => void;
+// }
 
-const CustomDataTable: React.FC<CustomDataTableProps> = ({
-  onSearch,
-  onSort,
-  ...restProps
-}) => {
-  return <DataTable {...restProps} />;
-};
+// const CustomDataTable: React.FC<CustomDataTableProps> = ({
+//   onSearch,
+//   onSort,
+//   ...restProps
+// }) => {
+//   return <DataTable {...restProps} />;
+// };
 
-export default CustomDataTable;
+// export default CustomDataTable;
--- components/tables/recent-order-tables/customDataTable.tsx
+++ components/tables/recent-order-tables/customDataTable.tsx
@@ -1,20 +1,20 @@
-// CustomDataTable.tsx
+// // CustomDataTable.tsx
 
-import React from 'react';
-import { DataTable, DataTableProps } from '@tanstack/react-table';
-import { OrderManagement } from '@/constants/order-management-data';
+// import React from 'react';
+// import { DataTable, DataTableProps } from '@tanstack/react-table';
+// import { OrderManagement } from '@/constants/order-management-data';
 
-interface CustomDataTableProps extends DataTableProps<OrderManagement, unknown> {
-  onSearch: (searchValue: string) => void;
-  onSort: (sortBy: keyof OrderManagement, sortOrder: 'asc' | 'desc') => void;
-}
+// interface CustomDataTableProps extends DataTableProps<OrderManagement, unknown> {
+//   onSearch: (searchValue: string) => void;
+//   onSort: (sortBy: keyof OrderManagement, sortOrder: 'asc' | 'desc') => void;
+// }
 
-const CustomDataTable: React.FC<CustomDataTableProps> = ({
-  onSearch,
-  onSort,
-  ...restProps
-}) => {
-  return <DataTable {...restProps} />;
-};
+// const CustomDataTable: React.FC<CustomDataTableProps> = ({
+//   onSearch,
+//   onSort,
+//   ...restProps
+// }) => {
+//   return <DataTable {...restProps} />;
+// };
 
-export default CustomDataTable;
+// export default CustomDataTable;
