c68b04592e722dc416f3b810d128dcfdb2aa0090
Ridhi Priya <106872423+Ridhipriya85@users.noreply.github.com>
2024-07-24 17:02:31 +0530

    subscription column change

--- components/tables/subscription/columns.tsx
+++ components/tables/subscription/columns.tsx
@@ -9,73 +9,34 @@ import { CellAction } from './cell-action';
 export const columns: ColumnDef<Subscription>[] = [
   {
     accessorKey: 'subType',
-    header: ({ column, table }) => (
-      <div className="flex items-center">
-      
-        <Select
-          onValueChange={(value) => {
-            table.options.meta.updateColumnData(column.id, value);
-          }}
-        >
-          <SelectTrigger>
-            <SelectValue placeholder="Select a sub type" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value="Trial">Trial</SelectItem>
-            <SelectItem value="Monthly">Monthly</SelectItem>
-            <SelectItem value="Quarterly">Quarterly</SelectItem>
-            <SelectItem value="Semi Annual">Semi Annual</SelectItem>
-            <SelectItem value="Annual">Annual</SelectItem>
-          </SelectContent>
-        </Select>
-      </div>
-    ),
+    header: 'Sub Type',
     cell: ({ row, column, table }) => (
-      <div>{row.getValue(column.id)}</div>
+      <Select
+        value={row.getValue(column.id)}
+        onValueChange={(value) => {
+          table.options.meta.updateData(row.index, column.id, value);
+        }}
+      >
+        <SelectTrigger>
+          <SelectValue placeholder="Select a sub type" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="Trial">Trial</SelectItem>
+          <SelectItem value="Monthly">Monthly</SelectItem>
+          <SelectItem value="Quarterly">Quarterly</SelectItem>
+          <SelectItem value="Semi Annual">Semi Annual</SelectItem>
+          <SelectItem value="Annual">Annual</SelectItem>
+        </SelectContent>
+      </Select>
     ),
   },
   {
     accessorKey: 'noOfBag',
-    header: ({ column, table }) => (
-      <div className="flex items-center">
-       
-        <Input
-          type="number"
-          placeholder="No of Bag"
-          onChange={(e) => {
-            table.options.meta.updateColumnData(column.id, e.target.value);
-          }}
-        />
-      </div>
-    ),
+    header: 'No of Bag',
     cell: ({ row, column, table }) => (
       <Input
         type="number"
-        value={row.original.noOfBag}
-        onChange={(e) => {
-          table.options.meta.updateData(row.index, column.id, e.target.value);
-        }}
-      />
-    ),
-  },
-  {
-    accessorKey: 'price',
-    header: ({ column, table }) => (
-      <div className="flex items-center">
-       
-        <Input
-          type="number"
-          placeholder="Price"
-          onChange={(e) => {
-            table.options.meta.updateColumnData(column.id, e.target.value);
-          }}
-        />
-      </div>
-    ),
-    cell: ({ row, column, table }) => (
-      <Input
-        type="number"
-        value={row.original.price}
+        value={row.getValue(column.id)}
         onChange={(e) => {
           table.options.meta.updateData(row.index, column.id, e.target.value);
         }}
@@ -84,22 +45,11 @@ export const columns: ColumnDef<Subscription>[] = [
   },
   {
     accessorKey: 'pricePercentageSeasonalVeggies',
-    header: ({ column, table }) => (
-      <div className="flex items-center">
-       
-        <Input
-          type="number"
-          placeholder="Percentage of Seasonal Veggies"
-          onChange={(e) => {
-            table.options.meta.updateColumnData(column.id, e.target.value);
-          }}
-        />
-      </div>
-    ),
+    header: 'Price Percentage of Seasonal Veggies',
     cell: ({ row, column, table }) => (
       <Input
         type="number"
-        value={row.original.pricePercentageSeasonalVeggies}
+        value={row.getValue(column.id)}
         onChange={(e) => {
           table.options.meta.updateData(row.index, column.id, e.target.value);
         }}
