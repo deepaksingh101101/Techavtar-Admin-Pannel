c52074f01c1298c9a0c8cf4fdcafe62c0e03cfb1
Deepak Singh <deepaksingh101101@gmail.com>
2024-07-30 19:31:44 +0530

    order completed

--- components/forms/order-stepper/create-order.tsx
+++ components/forms/order-stepper/create-order.tsx
@@ -9,13 +9,16 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@
 import { Separator } from '@/components/ui/separator';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Trash } from 'lucide-react';
+import { Trash, CalendarIcon } from 'lucide-react';
 import { useParams, useRouter } from 'next/navigation';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { SubmitHandler, useForm, Controller } from 'react-hook-form';
 import { cn } from '@/lib/utils';
-import { MultiSelect } from '@/components/ui/MultiSelect';
 import { Textarea } from '@/components/ui/textarea';
+import ReactSelect from 'react-select';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import { Calendar } from '@/components/ui/calendar';
+import { format } from 'date-fns';
 
 interface OrderManagementFormType {
   initialData: any | null;
@@ -23,8 +26,10 @@ interface OrderManagementFormType {
 
 const orderFormSchema = z.object({
   customerName: z.string().min(1, 'Customer Name is required'),
-  eid: z.number().min(1, 'Eid is required'),
-  deliveryDate: z.string().min(1, 'Delivery Date is required'),
+  employeeName: z.string().min(1, 'Employee Name is required'),
+  deliveryDate: z.date({
+    required_error: "Delivery Date is required.",
+  }),
   deliveryTimeSlot: z.string().min(1, 'Delivery Time Slot is required'),
   deliveryStatus: z.string(),
   bagOrdered: z.array(z.string()).min(1, 'Products Ordered is required'),
@@ -48,7 +53,7 @@ const steps: Step[] = [
     name: 'Order Details',
     fields: [
       'customerName',
-      'eid',
+      'employeeName',
       'deliveryDate',
       'deliveryTimeSlot',
       'deliveryStatus',
@@ -68,11 +73,19 @@ const steps: Step[] = [
   { id: 'Step 3', name: 'Complete' } // No fields needed here
 ];
 
+const dummyBags = [
+  { value: 'Regular Veggie Bag', label: 'Regular Veggie Bag', weight: 4000 },
+  { value: 'Mini Veggie Bag', label: 'Mini Veggie Bag', weight: 3000 },
+  { value: 'Large Veggie Bag', label: 'Large Veggie Bag', weight: 5000 },
+  { value: 'Veggie Bag', label: 'Veggie Bag', weight: 5000 }
+];
+
 export const CreateOrder: React.FC<OrderManagementFormType> = ({ initialData }) => {
   const params = useParams();
   const router = useRouter();
   const [open, setOpen] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [isTotalWeightValid, setIsTotalWeightValid] = useState(true);
   const title = initialData ? 'Edit Order' : 'Create New Order';
   const description = initialData ? 'Edit the Order details.' : 'To create a new Order, fill in the required information.';
   const toastMessage = initialData ? 'Order updated.' : 'Order created.';
@@ -83,8 +96,8 @@ export const CreateOrder: React.FC<OrderManagementFormType> = ({ initialData }) 
     mode: 'onChange',
     defaultValues: {
       customerName: '',
-      eid: undefined as any, // Initialize as any to handle initial state
-      deliveryDate: '',
+      employeeName: '',
+      deliveryDate: new Date(),
       deliveryTimeSlot: '',
       deliveryStatus: 'Pending',
       bagOrdered: [] as string[],
@@ -95,7 +108,7 @@ export const CreateOrder: React.FC<OrderManagementFormType> = ({ initialData }) 
     }
   });
 
-  const { control, trigger, watch, handleSubmit, formState: { errors } } = form;
+  const { control, trigger, watch, handleSubmit, setValue, formState: { errors } } = form;
 
   const onSubmit: SubmitHandler<typeof orderFormSchema._type> = async (data) => {
     try {
@@ -117,9 +130,9 @@ export const CreateOrder: React.FC<OrderManagementFormType> = ({ initialData }) 
   const onDelete = async () => {
     try {
       setLoading(true);
-      //   await axios.delete(`/api/${params.storeId}/orders/${params.orderId}`);
+      //   await axios.delete(`/api/${params.storemployeeName}/orders/${params.orderId}`);
       // router.refresh();
-      // router.push(`/${params.storeId}/orders`);
+      // router.push(`/${params.storemployeeName}/orders`);
     } catch (error: any) {
     } finally {
       setLoading(false);
@@ -154,6 +167,33 @@ export const CreateOrder: React.FC<OrderManagementFormType> = ({ initialData }) 
     { id: '1', name: 'Mini Veggie Bag' },
     { id: '2', name: 'Regular Veggie Bag' },
   ];
+
+  const employeeOptions = [
+    { id: '1', name: 'John Doe', phoneNumber: '123-456-7890' },
+    { id: '2', name: 'Jane Smith', phoneNumber: '098-765-4321' },
+  ];
+
+  const customerOptions = [
+    { id: '1', name: 'Alice Johnson', phoneNumber: '123-456-7890' },
+    { id: '2', name: 'Bob Brown', phoneNumber: '098-765-4321' },
+  ];
+
+  const selectedBags = watch('bagOrdered');
+
+  useEffect(() => {
+    const totalWeight = selectedBags.reduce((acc, bagName) => {
+      const bag = dummyBags.find((bag) => bag.value === bagName);
+      return acc + (bag ? bag.weight : 0);
+    }, 0);
+
+    setValue('totalWeight', totalWeight);
+
+    if (totalWeight >= 5000 && totalWeight <= 10000) {
+      setIsTotalWeightValid(true);
+    } else {
+      setIsTotalWeightValid(false);
+    }
+  }, [selectedBags, setValue]);
 
   return (
     <>
@@ -219,35 +259,57 @@ export const CreateOrder: React.FC<OrderManagementFormType> = ({ initialData }) 
           >
             {currentStep === 0 && (
               <>
-                <FormField
+                <Controller
                   control={form.control}
                   name="customerName"
                   render={({ field }) => (
                     <FormItem>
                       <FormLabel>Customer Name</FormLabel>
                       <FormControl>
-                        <Input type="text" disabled={loading} placeholder="Enter Customer Name" {...field} />
+                        <ReactSelect
+                          isClearable
+                          isSearchable
+                          options={customerOptions}
+                          getOptionLabel={(option) => option.name}
+                          getOptionValue={(option) => option.id}
+                          isDisabled={loading}
+                          onChange={(selected) => field.onChange(selected ? selected.name : '')}
+                          value={customerOptions.find(option => option.name === field.value)}
+                          filterOption={(candidate, input) => {
+                            const customer = customerOptions.find(cust => cust.id === candidate.value);
+                            return candidate.label.toLowerCase().includes(input.toLowerCase()) ||
+                              (customer?.phoneNumber.includes(input) ?? false);
+                          }}
+                        />
                       </FormControl>
                       <FormMessage>{errors.customerName?.message}</FormMessage>
                     </FormItem>
                   )}
                 />
-                <FormField
+                <Controller
                   control={form.control}
-                  name="eid"
+                  name="employeeName"
                   render={({ field }) => (
                     <FormItem>
-                      <FormLabel>Employee Id</FormLabel>
+                      <FormLabel>Employee Name</FormLabel>
                       <FormControl>
-                        <Input
-                          type="number"
-                          disabled={loading}
-                          placeholder="Enter Employee Id"
-                          onChange={(e) => field.onChange(e.target.value === '' ? undefined : Number(e.target.value))}
-                          value={field.value || ''}
+                        <ReactSelect
+                          isClearable
+                          isSearchable
+                          options={employeeOptions}
+                          getOptionLabel={(option) => option.name}
+                          getOptionValue={(option) => option.id}
+                          isDisabled={loading}
+                          onChange={(selected) => field.onChange(selected ? selected.name : '')}
+                          value={employeeOptions.find(option => option.name === field.value)}
+                          filterOption={(candidate, input) => {
+                            const employee = employeeOptions.find(emp => emp.id === candidate.value);
+                            return candidate.label.toLowerCase().includes(input.toLowerCase()) ||
+                              (employee?.phoneNumber.includes(input) ?? false);
+                          }}
                         />
                       </FormControl>
-                      {/* <FormMessage>{errors.eid?.message}</FormMessage> */}
+                      <FormMessage>{errors.employeeName?.message}</FormMessage>
                     </FormItem>
                   )}
                 />
@@ -255,15 +317,39 @@ export const CreateOrder: React.FC<OrderManagementFormType> = ({ initialData }) 
                   control={form.control}
                   name="deliveryDate"
                   render={({ field }) => (
-                    <FormItem>
+                    <FormItem className="flex flex-col">
                       <FormLabel>Delivery Date</FormLabel>
-                      <FormControl>
-                        <Input
-                          type="date"
-                          disabled={loading}
-                          {...field}
-                        />
-                      </FormControl>
+                      <Popover>
+                        <PopoverTrigger asChild>
+                          <FormControl>
+                            <Button
+                              variant={"outline"}
+                              className={cn(
+                                "w-[240px] pl-3 text-left font-normal",
+                                !field.value && "text-muted-foreground"
+                              )}
+                            >
+                              {field.value ? (
+                                format(field.value, "dd MMM yyyy")
+                              ) : (
+                                <span>Pick a date</span>
+                              )}
+                              <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                            </Button>
+                          </FormControl>
+                        </PopoverTrigger>
+                        <PopoverContent className="w-auto p-0" align="start">
+                          <Calendar
+                            mode="single"
+                            selected={field.value}
+                            onSelect={field.onChange}
+                            disabled={(date) =>
+                              date > new Date() || date < new Date("1900-01-01")
+                            }
+                            initialFocus
+                          />
+                        </PopoverContent>
+                      </Popover>
                       <FormMessage />
                     </FormItem>
                   )}
@@ -323,15 +409,28 @@ export const CreateOrder: React.FC<OrderManagementFormType> = ({ initialData }) 
                     <FormItem>
                       <FormLabel>Bag Ordered</FormLabel>
                       <FormControl>
-                        <MultiSelect
-                          value={field.value || []}
-                          onChange={(value) => field.onChange(value)}
-                          options={orderedOptions}
-                          disabled={loading}
-                          placeholder="Select Bag Options"
+                        <ReactSelect
+                          isMulti
+                          isClearable
+                          isSearchable
+                          options={dummyBags}
+                          formatOptionLabel={(option) => (
+                            <div className="flex justify-between items-center">
+                              <span>{option.label}</span>
+                              <span className=" ms-4 text-green-700">{option.weight}g</span>
+                            </div>
+                          )}
+                          onChange={(selected) => {
+                            field.onChange(selected ? selected.map(option => option.value) : []);
+                          }}
+                          value={dummyBags.filter(option => field.value.includes(option.value))}
                         />
                       </FormControl>
-                      <FormMessage />
+                      {!isTotalWeightValid && (
+                        <FormMessage>
+                          <span className="text-red-500">Total weight must be between 5000 to 10000 grams</span>
+                        </FormMessage>
+                      )}
                     </FormItem>
                   )}
                 />
@@ -340,13 +439,13 @@ export const CreateOrder: React.FC<OrderManagementFormType> = ({ initialData }) 
                   name="totalWeight"
                   render={({ field }) => (
                     <FormItem>
-                      <FormLabel>Total Weight (kg)</FormLabel>
+                      <FormLabel>Total Weight (g)</FormLabel>
                       <FormControl>
                         <Input
                           type="number"
                           placeholder="Enter Total Weight"
-                          onChange={(e) => field.onChange(e.target.value === '' ? undefined : Number(e.target.value))}
-                          value={field.value || ''}
+                          value={field.value}
+                          readOnly
                         />
                       </FormControl>
                       <FormMessage />
@@ -479,25 +578,27 @@ export const CreateOrder: React.FC<OrderManagementFormType> = ({ initialData }) 
             </svg>
           </button>
           <button
-            type="button"
-            onClick={next}
-            className="rounded bg-white px-2 py-1 text-sm font-semibold text-sky-900 shadow-sm ring-1 ring-inset ring-sky-300 hover:bg-sky-50 disabled:cursor-not-allowed disabled:opacity-50"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="h-6 w-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M8.25 4.5l7.5 7.5-7.5 7.5"
-              />
-            </svg>
-          </button>
+  type="button"
+  onClick={next}
+  className="rounded bg-white px-2 py-1 text-sm font-semibold text-sky-900 shadow-sm ring-1 ring-inset ring-sky-300 hover:bg-sky-50 disabled:cursor-not-allowed disabled:opacity-50"
+  disabled={!isTotalWeightValid}
+>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth="1.5"
+    stroke="currentColor"
+    className="h-6 w-6"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M8.25 4.5l7.5 7.5-7.5 7.5"
+    />
+  </svg>
+</button>
+
         </div>
       </div>
     </>
